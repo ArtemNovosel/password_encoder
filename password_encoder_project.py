56c351594d90e3f763389f6072aa288301824768
ArtemNovosel <arteimn@mail.ru>
2023-02-02 19:53:58 +0500

    'import_re'

--- password_encoder_project.py
+++ password_encoder_project.py
@@ -1,11 +1,21 @@
+import re
 password_list = []
 password = input('Введите символы: ')  # получаем пароль от пользователя
-for i in password:  # в цикле к каждое значение уваличиваем на 100
-    if i.isdigit():
-      password_list.append(int(i) + 100)  # записываем результат в password_list
+chisla = re.findall(r'\d*\d+|\d+', password)
+chisla = [int(i) for i in chisla] # вычленяем из пароля числа
+# print(chisla)
+b = -1
+c = 0
+for i in password:
+    if i.isdigit():   # натыкаясь на цифру добавляем в список число
+        while c < 1:
+            b = b + 1   # которое соответствует колличеству проходов b
+            password_list.append(chisla[b]+100)
+            c = 1             # игнорим остальные цифры в числе
     else:
-      password_list.append('100' + i)  # записываем результат в password_list
+        c = 0   # включаем условие для блока цифр
+        password_list.append(i)
 # print(password_list)
-kod_password = str(password_list).replace(", ", "")  # password_list преобразовываем в строку удалив лишние символы
-kod_password = kod_password[1:-1].replace("'","")
-print("Кодированный пароль: ", kod_password)
+kod_password = str(password_list).replace(", ", "")
+kod_password = kod_password[1:-1].replace("'", "")
+print(kod_password)
