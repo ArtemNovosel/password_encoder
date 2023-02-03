ae7dfb4a9e6b4a29271151547e52efbad75e1efb
ArtemNovosel <arteimn@mail.ru>
2023-02-03 19:21:13 +0500

    'valid_list'

--- password_encoder_project.py
+++ password_encoder_project.py
@@ -1,21 +1,34 @@
 import re
 password_list = []
-password = input('Введите символы: ')  # получаем пароль от пользователя
+password = input('Введите пароль: ')  # получаем пароль от пользователя
+# print(len(password))
+
 chisla = re.findall(r'\d*\d+|\d+', password)
-chisla = [int(i) for i in chisla] # вычленяем из пароля числа
+chisla = [int(i) for i in chisla]  # вычленяем из пароля числа
 # print(chisla)
-b = -1
-c = 0
-for i in password:
-    if i.isdigit():   # натыкаясь на цифру добавляем в список число
-        while c < 1:
-            b = b + 1   # которое соответствует колличеству проходов b
-            password_list.append(chisla[b]+100)
-            c = 1             # игнорим остальные цифры в числе
-    else:
-        c = 0   # включаем условие для блока цифр
-        password_list.append(i)
-# print(password_list)
-kod_password = str(password_list).replace(", ", "")
-kod_password = kod_password[1:-1].replace("'", "")
-print(kod_password)
+
+valid_list = ['/', ',', '.', ' ', "'", '-', '"']  # вносим символы в список исключений
+a = [i for i in password if i in valid_list]  # колличество невалидных символов в password
+# print(a)
+if a:
+    print('Вы использовали запрещенные символы: ', *a)
+
+# /дальше блок преобразования
+else:
+    b = -1
+    c = 0
+    for i in password:
+        if i.isdigit():  # натыкаясь на цифру добавляем в список число
+            while c < 1:
+                b = b + 1  # которое соответствует колличеству проходов b
+                password_list.append(chisla[b] + 100)
+                c = 1  # игнорим остальные цифры в числе
+        else:
+            c = 0  # включаем условие для блока цифр
+            password_list.append(i)
+    # print(password_list)
+    kod_password = str(password_list).replace(", ", "")
+    kod_password = kod_password[1:-1].replace("'", "")
+    print(kod_password)
+# else:
+#     print("Вы использовали запрещенные символы")
