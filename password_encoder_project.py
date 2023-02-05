5bd26f401cb6cec1ca31ff4245c265588634339c
ArtemNovosel <arteimn@mail.ru>
2023-02-05 20:50:24 +0500

    'functions'

--- password_encoder_project.py
+++ password_encoder_project.py
@@ -1,34 +1,22 @@
-import re
-password_list = []
-password = input('Введите пароль: ')  # получаем пароль от пользователя
-# print(len(password))
-
-chisla = re.findall(r'\d*\d+|\d+', password)
-chisla = [int(i) for i in chisla]  # вычленяем из пароля числа
-# print(chisla)
-
-valid_list = ['/', ',', '.', ' ', "'", '-', '"']  # вносим символы в список исключений
-a = [i for i in password if i in valid_list]  # колличество невалидных символов в password
-# print(a)
-if a:
-    print('Вы использовали запрещенные символы: ', *a)
-
-# /дальше блок преобразования
-else:
-    b = -1
-    c = 0
-    for i in password:
-        if i.isdigit():  # натыкаясь на цифру добавляем в список число
-            while c < 1:
-                b = b + 1  # которое соответствует колличеству проходов b
-                password_list.append(chisla[b] + 100)
-                c = 1  # игнорим остальные цифры в числе
+valid_list = ['/', ',', '.', ' ', "'", '-', '"', '']  # вносим символы в список исключений
+def valid_password(password):
+        a = [i for i in password if i in valid_list]
+        if a:
+            return f'Вы использовали запрещенные символы:{a}'
         else:
-            c = 0  # включаем условие для блока цифр
-            password_list.append(i)
-    # print(password_list)
-    kod_password = str(password_list).replace(", ", "")
-    kod_password = kod_password[1:-1].replace("'", "")
-    print(kod_password)
-# else:
-#     print("Вы использовали запрещенные символы")
+            return password
+def koder(fn):
+    pasvord = fn
+    b = []
+    a = 0
+    for i in pasvord+" ":
+        if i.isdigit():
+            a = a * 10 + int(i)
+        elif a != 0:
+            b.append(str(a + 100))
+            a = 0
+            b.append(i)
+        else:
+            b.append(i)
+    return ''.join(b[0:-1])
+print(koder(valid_password(input('Введите пароль: '))))
